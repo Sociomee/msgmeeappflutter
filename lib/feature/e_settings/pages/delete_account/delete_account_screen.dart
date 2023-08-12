ec63003fc6bd75a36d402749893bb426c55f91ae
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-08-12 16:02:00 +0530

    fixing delete account page issues

--- lib/feature/e_settings/pages/delete_account/delete_account_screen.dart
+++ lib/feature/e_settings/pages/delete_account/delete_account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:msgmee/common_widgets/custom_button_widget.dart';
 import 'package:msgmee/feature/e_settings/pages/delete_account/widget/delete_account_dialog.dart';
 
@@ -50,122 +51,139 @@ class _DeleterAccountScreenState extends State<DeleterAccountScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back_ios,
-              color: AppColors.black,
-            )),
-        title: Text(
-          'Delate my account',
-          style: TextStyle(color: AppColors.black),
-        ),
-      ),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Icon(Icons.arrow_back_ios,
+                      color: AppColors.black, size: 20))),
+          leadingWidth: 40,
+          titleSpacing: 6,
+          title: Text('Delete my account',
+              style: TextStyle(
+                  color: AppColors.black,
+                  fontSize: 18,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600))),
       body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 30),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/error.png',
-                          height: 25,
-                        ),
-                        SizedBox(width: 20),
-                        Text(
-                          'By deleting your account, following\nactions will happen ! ',
-                          style: TextStyle(
+                    SvgPicture.asset('assets/error_icon.svg'),
+                    SizedBox(width: 12),
+                    Text(
+                        'By deleting your account, following\nactions will happen ! ',
+                        style: TextStyle(
                             color: Colors.red,
                             fontSize: 16,
                             fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
+                            fontWeight: FontWeight.w500))
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.only(top: 5, left: 50.w, bottom: 5),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Padding(
-                      padding:
-                          const EdgeInsets.only(top: 5, left: 44, bottom: 5),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 6,
-                            width: 6,
-                            decoration: BoxDecoration(
-                                color: AppColors.iconColor,
-                                borderRadius: BorderRadius.circular(100)),
-                          ),
-                          SizedBox(width: 8),
-                          Text('Message history will be erased.')
-                        ],
-                      ),
+                      padding: const EdgeInsets.only(top: 3.0),
+                      child: Icon(Icons.fiber_manual_record,
+                          color: AppColors.iconColor, size: 7),
                     ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 5, left: 44, bottom: 5),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 6,
-                            width: 6,
-                            decoration: BoxDecoration(
-                                color: AppColors.iconColor,
-                                borderRadius: BorderRadius.circular(100)),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                              'You will be deleted from all your MsgMee\ngroups.')
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 5, left: 44, bottom: 5),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 6,
-                            width: 6,
-                            decoration: BoxDecoration(
-                                color: AppColors.iconColor,
-                                borderRadius: BorderRadius.circular(100)),
-                          ),
-                          SizedBox(width: 8),
-                          Text('All your Google Drive backup will be deleted.')
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 35),
-                    Text('Reason For Deletion(Optional)',
+                    SizedBox(width: 8),
+                    Text('Message history will be erased.',
                         style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 5),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          contentPadding:
-                              EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                          border: OutlineInputBorder(),
-                          hintText: 'Type your reason (optional)...'),
+                            color: Color(0xFF555555),
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5, left: 50.w, bottom: 5),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4.0),
+                      child: Icon(Icons.fiber_manual_record,
+                          color: AppColors.iconColor, size: 7),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'Reason For Deletion(Optional)',
+                      style: TextStyle(
+                        color: Color(0xFF333333),
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
                     )
-                  ]),
-            ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5, left: 50.w, bottom: 5),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 3.0),
+                      child: Icon(Icons.fiber_manual_record,
+                          color: AppColors.iconColor, size: 7),
+                    ),
+                    SizedBox(width: 8),
+                    Text('All your Google Drive backup will be deleted.',
+                        style: TextStyle(
+                            color: Color(0xFF555555),
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400))
+                  ],
+                ),
+              ),
+              SizedBox(height: 35),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text('Reason For Deletion(Optional)',
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+              ),
+              SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                        border: OutlineInputBorder(),
+                        hintText: 'Type your reason...',
+                        hintStyle: TextStyle(
+                            color: Color(0xFF999999),
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400))),
+              )
+            ]),
             SizedBox(height: 20),
             Container(
-              height: 82,
               width: MediaQuery.of(context).size.width,
               padding: EdgeInsets.all(20),
               alignment: Alignment.centerLeft,
-              color: AppColors.lightgrey.withOpacity(.4),
+              color: Color(0xFFF6F6F6),
               child: Text(
                 'To deleted your account, confirm your country code\nand enter your phone number ',
                 style: TextStyle(color: AppColors.black, fontSize: 14),
@@ -189,10 +207,12 @@ class _DeleterAccountScreenState extends State<DeleterAccountScreen> {
                   LengthLimitingTextInputFormatter(10),
                 ],
                 decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    border: OutlineInputBorder(),
-                    hintText: '(+91)  9876543210'),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  border: OutlineInputBorder(),
+                  hintText: '(+91)  9876543210',
+                  hintStyle: TextStyle(color: Color(0xFF999999)),
+                ),
               ),
             ),
             Padding(
@@ -205,10 +225,14 @@ class _DeleterAccountScreenState extends State<DeleterAccountScreen> {
               child: TextFormField(
                 controller: passwordController,
                 decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter password'),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter password',
+                  hintStyle: TextStyle(
+                    color: Color(0xFF999999),
+                  ),
+                ),
               ),
             ),
             SizedBox(height: 70.h),
