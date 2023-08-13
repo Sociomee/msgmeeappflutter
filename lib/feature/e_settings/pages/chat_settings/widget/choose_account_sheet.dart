9c8c86f4f039b576ab6acbb6fe73ac21cdda20c3
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-08-14 01:05:30 +0530

    fixing choose account section

--- lib/feature/e_settings/pages/chat_settings/widget/choose_account_sheet.dart
+++ lib/feature/e_settings/pages/chat_settings/widget/choose_account_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../../../../../theme/colors.dart';
 
 class OptionsModel {
@@ -7,41 +8,52 @@ class OptionsModel {
   final String title;
   final String subtitle;
   bool isSelected;
-
+  GoogleAccountOption value;
   OptionsModel({
-    required this.title,
-    required this.isSelected,
     required this.imageUrl,
+    required this.title,
     required this.subtitle,
+    required this.isSelected,
+    required this.value,
   });
 }
 
 List<OptionsModel> options = [
   OptionsModel(
-      title: 'Savannah Nguyen',
-      isSelected: false,
-      imageUrl:
-          'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      subtitle: 'savannah_nguyen123@gmail.com'),
+    title: 'Savannah Nguyen',
+    isSelected: false,
+    imageUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'savannah_nguyen123@gmail.com',
+    value: GoogleAccountOption.ac1,
+  ),
   OptionsModel(
-      title: 'Wade Warren',
-      isSelected: false,
-      imageUrl:
-          'https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      subtitle: 'dummy_email@gmail.com'),
+    title: 'Wade Warren',
+    isSelected: false,
+    imageUrl:
+        'https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'dummy_email@gmail.com',
+    value: GoogleAccountOption.ac2,
+  ),
   OptionsModel(
-      title: 'Annette Black',
-      isSelected: false,
-      imageUrl:
-          'https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      subtitle: 'dummy_email@gmail.com'),
+    title: 'Annette Black',
+    isSelected: false,
+    imageUrl:
+        'https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'dummy_email@gmail.com',
+    value: GoogleAccountOption.ac3,
+  ),
   OptionsModel(
-      title: 'Marvin McKinney',
-      isSelected: true,
-      imageUrl:
-          'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      subtitle: 'dummy_email@gmail.com'),
+    title: 'Marvin McKinney',
+    isSelected: true,
+    imageUrl:
+        'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'dummy_email@gmail.com',
+    value: GoogleAccountOption.ac4,
+  ),
 ];
+
+enum GoogleAccountOption { ac1, ac2, ac3, ac4 }
 
 class ChooseAccountBottomSheet extends StatefulWidget {
   const ChooseAccountBottomSheet({super.key});
@@ -52,6 +64,7 @@ class ChooseAccountBottomSheet extends StatefulWidget {
 }
 
 class _ChooseAccountBottomSheetState extends State<ChooseAccountBottomSheet> {
+  GoogleAccountOption? groupvalue = GoogleAccountOption.ac1;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -60,7 +73,7 @@ class _ChooseAccountBottomSheetState extends State<ChooseAccountBottomSheet> {
           topRight: Radius.circular(20.0),
         ),
         child: Container(
-          height: 380.h,
+          height: 415.h,
           decoration: BoxDecoration(
               color: AppColors.white, borderRadius: BorderRadius.circular(25)),
           child: Column(
@@ -85,16 +98,16 @@ class _ChooseAccountBottomSheetState extends State<ChooseAccountBottomSheet> {
                     return Column(
                       children: [
                         Container(
-                          color: options[index].isSelected
+                          color: groupvalue == options[index].value
                               ? AppColors.seconderyColor1
                               : Colors.transparent,
                           child: ListTile(
                             tileColor: AppColors.seconderyColor,
                             selectedTileColor: AppColors.seconderyColor,
                             onTap: () {
-                              options[index].isSelected =
-                                  !options[index].isSelected;
-                              setState(() {});
+                              setState(() {
+                                groupvalue = options[index].value;
+                              });
                             },
                             leading: CircleAvatar(
                               radius: 28,
@@ -102,53 +115,41 @@ class _ChooseAccountBottomSheetState extends State<ChooseAccountBottomSheet> {
                               backgroundImage:
                                   NetworkImage(options[index].imageUrl),
                             ),
-                            title: Text(
-                              options[index].title,
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 14,
-                              ),
-                            ),
-                            subtitle: Text(
-                              options[index].subtitle,
-                              style: TextStyle(
-                                color: AppColors.iconColor,
-                                fontSize: 11,
-                              ),
-                            ),
-                            trailing: Container(
-                              height: 20,
-                              width: 20,
-                              decoration: BoxDecoration(
-                                  color: AppColors.white,
-                                  border: Border.all(
-                                      color: AppColors.primaryColor, width: 1),
-                                  borderRadius: BorderRadius.circular(100)),
-                              child: options[index].isSelected
-                                  ? Container(
-                                      height: 5,
-                                      width: 5,
-                                      decoration: BoxDecoration(
-                                          color: AppColors.primaryColor,
-                                          border: Border.all(
-                                              color: AppColors.white, width: 3),
-                                          borderRadius:
-                                              BorderRadius.circular(100)),
-                                    )
-                                  : Container(),
-                            ),
+                            title: Text(options[index].title,
+                                style: TextStyle(
+                                    color: Colors.blue, fontSize: 14)),
+                            subtitle: Text(options[index].subtitle,
+                                style: TextStyle(
+                                    color: AppColors.iconColor, fontSize: 11)),
+                            trailing: Radio<GoogleAccountOption>(
+                                fillColor: MaterialStateColor.resolveWith(
+                                    (states) =>
+                                        groupvalue == options[index].value
+                                            ? AppColors.primaryColor
+                                            : AppColors.lightgrey),
+                                activeColor: AppColors.primaryColor,
+                                value: options[index].value,
+                                groupValue: groupvalue,
+                                onChanged: (GoogleAccountOption? e) {
+                                  setState(() {
+                                    groupvalue = e;
+                                  });
+                                }),
                           ),
                         ),
                         Divider(
-                          color: AppColors.primaryColor,
+                          color: AppColors.lightgrey,
                           height: 0,
+                          thickness: 1,
                         ),
                       ],
                     );
                   }),
               SizedBox(height: 24),
               TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                   child: Text(
                     'Add new account',
                     style:
