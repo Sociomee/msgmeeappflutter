784e7fc562aee929b8118c02b462fb77944e5e56
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-08-17 16:00:40 +0530

    fixing scan qr page issue

--- lib/feature/e_settings/pages/connect_webpanel/connect_webpanel_screen.dart
+++ lib/feature/e_settings/pages/connect_webpanel/connect_webpanel_screen.dart
@@ -8,20 +8,30 @@ class ConncetWenPanelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          elevation: 1,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: AppColors.black,
-              )),
-          title: Text(
-            'Scan QR code',
-            style: TextStyle(color: AppColors.black),
-          ),
-        ),
+            elevation: 1,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: AppColors.black,
+                    size: 17,
+                  ),
+                )),
+            leadingWidth: 40,
+            titleSpacing: 0,
+            title: Text(
+              'Scan QR',
+              style: TextStyle(
+                color: Color(0xFF333333),
+                fontSize: 18,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+              ),
+            )),
         body: Container(
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(color: AppColors.black.withOpacity(0.6)),
@@ -29,13 +39,17 @@ class ConncetWenPanelScreen extends StatelessWidget {
             Image.asset('assets/qr.png'),
             Spacer(),
             Padding(
-              padding: const EdgeInsets.only(bottom: 47, left: 47, right: 47),
-              child: Text(
-                'Open web.sociomee.com, desktop app, or other devices.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: AppColors.white),
-              ),
-            )
+                padding: const EdgeInsets.only(bottom: 47, left: 30, right: 30),
+                child: Text(
+                  'Open web.sociomee.com, desktop app, or other devices.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ))
           ]),
         ));
   }
