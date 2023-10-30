fa4b0f01d2f86e0ccb07be87aaa3caa663be44c7
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-10-30 15:17:27 +0530

    creating see more text feature

--- lib/feature/c_social_chat/presentation/pages/chat_screen/widgets/url_text_widget.dart
+++ lib/feature/c_social_chat/presentation/pages/chat_screen/widgets/url_text_widget.dart
@@ -1,21 +1,86 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:bot_toast/bot_toast.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:bot_toast/bot_toast.dart';
+// import 'package:flutter/gestures.dart';
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// class UrlTextWidget extends StatelessWidget {
+//   const UrlTextWidget({super.key, required this.text});
+//   final String text;
+//   @override
+//   Widget build(BuildContext context) {
+//     List<TextSpan> textSpans = [];
+//     int currentIndex = 0;
+//     RegExp urlRegExp = RegExp(
+//       r"https?://[^\s]+",
+//       caseSensitive: false,
+//     );
+//     urlRegExp.allMatches(text).forEach((match) {
+//       final url = match.group(0);
+//       final preMatch = text.substring(currentIndex, match.start);
+//       textSpans.add(
+//         TextSpan(text: preMatch),
+//       );
+//       textSpans.add(
+//         TextSpan(
+//           text: url,
+//           style: TextStyle(
+//               color: Color.fromARGB(255, 127, 197, 255),
+//               fontSize: 14), // Change the color to your desired color
+//           recognizer: TapGestureRecognizer()
+//             ..onTap = () async {
+//               log('clicking link..');
+
+//               if (!await launchUrl(Uri.parse(url!))) {
+//                 throw BotToast.showText(text: "could not launch this url!");
+//               }
+//             },
+//         ),
+//       );
+//       currentIndex = match.end;
+//     });
+
+//     if (currentIndex < text.length) {
+//       textSpans.add(
+//         TextSpan(text: text.substring(currentIndex)),
+//       );
+//     }
+
+//     return RichText(text: TextSpan(children: textSpans));
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:msgmee/theme/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class UrlTextWidget extends StatelessWidget {
-  const UrlTextWidget({super.key, required this.text});
+class UrlTextWidget extends StatefulWidget {
   final String text;
+  final int maxLines;
+
+  UrlTextWidget({Key? key, required this.text, this.maxLines = 11})
+      : super(key: key);
+
+  @override
+  _UrlTextWidgetState createState() => _UrlTextWidgetState();
+}
+
+class _UrlTextWidgetState extends State<UrlTextWidget> {
+  bool showMore = false;
+
   @override
   Widget build(BuildContext context) {
     List<TextSpan> textSpans = [];
     int currentIndex = 0;
+    final text = widget.text;
+
     RegExp urlRegExp = RegExp(
       r"https?://[^\s]+",
       caseSensitive: false,
     );
+
     urlRegExp.allMatches(text).forEach((match) {
       final url = match.group(0);
       final preMatch = text.substring(currentIndex, match.start);
@@ -26,15 +91,18 @@ class UrlTextWidget extends StatelessWidget {
         TextSpan(
           text: url,
           style: TextStyle(
-              color: Color.fromARGB(255, 127, 197, 255),
-              fontSize: 14), // Change the color to your desired color
+            color: Color.fromARGB(255, 127, 197, 255),
+            fontSize: 14,
+          ),
           recognizer: TapGestureRecognizer()
             ..onTap = () async {
-              log('clicking link..');
+              print('clicking link..');
 
-              if (!await launchUrl(Uri.parse(url!))) {
-                throw BotToast.showText(text: "could not launch this url!");
+              if (!await canLaunch(url!)) {
+                throw Exception("Could not launch this URL: $url");
               }
+
+              await launch(url);
             },
         ),
       );
@@ -47,6 +115,36 @@ class UrlTextWidget extends StatelessWidget {
       );
     }
 
-    return RichText(text: TextSpan(children: textSpans));
+    final richText = RichText(
+      text: TextSpan(children: textSpans),
+      maxLines: showMore ? null : widget.maxLines,
+      overflow: TextOverflow.clip,
+    );
+
+    final isTextOverflowed =
+        richText.text.toPlainText().split('\n').length > widget.maxLines;
+
+    return Column(
+      children: [
+        richText,
+        if (isTextOverflowed && !showMore)
+          Row(
+            children: [
+              Spacer(),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    showMore = true;
+                  });
+                },
+                child: Text(
+                  '...See More',
+                  style: TextStyle(color: AppColors.white, fontSize: 12),
+                ),
+              ),
+            ],
+          ),
+      ],
+    );
   }
 }
